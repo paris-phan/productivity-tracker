f4ff9b5762abab2dc1fc1a90bf2dccbf40230cb5
Paris Phan <37759794+paris-phan@users.noreply.github.com>
2024-12-27 20:02:15 -0500

    Xcode Project Environment Setup

---
+++ macos/ProductivityTracker/ProductivityTracker/ProductivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTrackerApp.swift
+//  ProductivityTracker
+//
+//  Created by Paris Phan on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macos/ProductivityTracker/ProductivityTrackerTests/ProductivityTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityTrackerTests.swift
+//  ProductivityTrackerTests
+//
+//  Created by Paris Phan on 12/27/24.
+//
+
+import Testing
+@testable import ProductivityTracker
+
+struct ProductivityTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
